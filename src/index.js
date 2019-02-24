d975015c448799f6a4b85540ac33cc5e3e06127f
Vlad Tataritsky <tataritskii@bk.ru>
2019-02-24 18:26:42 +0300

    money-exchange done

--- src/index.js
+++ src/index.js
@@ -2,69 +2,56 @@ module.exports = function makeExchange(currency) {
 
     var obj = {};
     var H, Q, D, N, P;
+    var currencyWithoutH = 0;
+    var currencyWithoutQ = 0;
+    var currencyWithoutD = 0;
+    var currencyWithoutN = 0;
 
     if (currency / 50 >= 1) {
         H = Math.trunc(currency / 50);
-        var currencyWithoutH = currency - H * 50;
+        currencyWithoutH = currency - H * 50;
         obj["H"] = H;
-
-
-    }
-    else{
+    } else {
         currencyWithoutH = currency;
-
     }
-
 
     if (currencyWithoutH / 25 >= 1) {
         Q = Math.trunc(currencyWithoutH / 25);
-        var currencyWithoutQ = currencyWithoutH - Q * 25;
+        currencyWithoutQ = currencyWithoutH - Q * 25;
         obj["Q"] = Q;
+    } else {
 
+        currencyWithoutQ = currencyWithoutH;
     }
-    else{
-        currencyWithoutH = currencyWithoutQ;
-    }
-
 
     if (currencyWithoutQ / 10 >= 1) {
         D = Math.trunc(currencyWithoutQ / 10);
-        var currencyWithoutD = currencyWithoutQ - D * 10;
+        currencyWithoutD = currencyWithoutQ - D * 10;
         obj["D"] = D;
-
+    } else {
+        currencyWithoutD = currencyWithoutQ;
     }
-    else{
-        currencyWithoutQ = currencyWithoutD;
-    }
-
 
     if (currencyWithoutD / 5 >= 1) {
         N = Math.trunc(currencyWithoutD / 5);
-        var currencyWithoutN = currencyWithoutD - N * 5;
+        currencyWithoutN = currencyWithoutD - N * 5;
         obj["N"] = N;
-
+    } else {
+        currencyWithoutN = currencyWithoutD;
     }
-    else{
-        currencyWithoutD = currencyWithoutN;
-    }
-
 
     if (currencyWithoutN >= 1) {
-        P = currencyWithoutN ;
+        P = currencyWithoutN;
         obj["P"] = P;
-
-
     }
-    if(currency > 10000){
 
+    if (currency > 10000) {
         obj = {error: "You are rich, my friend! We don't have so much coins for exchange"};
     }
-    if(currency < 1){
+    if (currency < 1) {
         obj = {};
     }
 
-
-
-    return obj
+    return obj;
 
 };
